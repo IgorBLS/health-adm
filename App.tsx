fd7ba2d99598d184d333a6c83d36c5ffce0520f1
IgorBLS <igor.brusamolin@gmail.com>
2022-08-15 08:28:27 -0300

    Código mais limpo

--- App.tsx
+++ App.tsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
-} from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+import React from "react";
+import {NativeBaseProvider, extendTheme} from "native-base";
 import Card from "./Card/Card";
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
-type MyThemeType = typeof theme;
-declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
-}
 export default function App() {
   return (
     <NativeBaseProvider>
